4f9c8ff5ebfc25fbb620548a3f2c4dd166d14be2
Lester <louis.tant@gmail.com>
2017-02-08 23:09:36 -0500

    Best graphics

--- results/myplot.py
+++ results/myplot.py
@@ -3,23 +3,30 @@ from matplotlib import style
 import numpy as np
 import sys
 
-def drawGraph(filename):
-	x,y = np.loadtxt(filename, 
-				   delimiter = ',',
-				   unpack = True)
-	plt.plot(x,y)
+def drawGraph(file1,file2):
+	x,y = np.loadtxt(str(file1)+".csv", 
+					 delimiter = ',',
+				   	 unpack = True)
 
-	plt.title('Epic Chart')
-	plt.ylabel('Y axis')
-	plt.xlabel('X axis')
+	x1,y1 = np.loadtxt(str(file2)+".csv", 
+					 delimiter = ',',
+				   	 unpack = True)
 
+	plt.plot(x,y,'r--',label=file1)
+	plt.plot(x1,y1,'b-',label=file2)
+
+	plt.title(str(file1)+" vs "+str(file2))
+	plt.ylabel('Vitesse dexecution (s)')
+	plt.xlabel('Nombre doperations')
+	plt.legend(loc='best')
+	
  	plt.show()
 
 def main(argv=None):
 	if argv is None:
 		argv = sys.argv
 
-	drawGraph(sys.argv[1])
+	drawGraph(sys.argv[1],sys.argv[2])
 
 if __name__ == "__main__":
     main()
