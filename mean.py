61e6b949c7b9248fcaadfaa598d7047eb6384e8c
Lester <louis.tant@gmail.com>
2017-02-15 21:22:08 -0500

    Func average

--- mean.py
+++ mean.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
+# -*- coding: utf-8 -*-	
 import sys
 import argparse
 import csv
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file",help="Path to the CSV file")
-parser.add_argument("num",help="Number of elements in the list")
-parser.add_argument("-m","--mean", help="Print mean", action="store_true")
+# parser.add_argument("num",help="Number of elements in the list")
 
 args = parser.parse_args()
 sys.setrecursionlimit(500000)
@@ -16,19 +14,19 @@ sys.setrecursionlimit(500000)
 def main(argv=None):
 	if argv is None:
 		argv = sys.argv
-	val = 0
-	cpt = 0
+	num = [1000,5000,10000,50000,100000,500000]
 	with open(args.file) as csvfile:
 		reader = csv.reader(csvfile, delimiter=',')
-		for row in reader:
-			if row[0] == args.num:
-				val = val + float(row[1])
-				cpt = cpt + 1
-
-	if args.mean:
-		print(val/cpt)
-
-
+		for n in num:
+			val = 0
+			cpt = 1
+			for row in reader:
+				if str(n) == row[0]:
+					val = val + float(row[1])
+					cpt = cpt + 1
+			print(n,val/cpt)
+			csvfile.seek(0)
+		
 if __name__ == "__main__":
     main()
 
